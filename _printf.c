6fe1855532d193e429f38ecd538f49360eef4344
farouk <farmo6995@gmail.com>
2023-09-18 08:35:42 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -22,24 +22,28 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == 'c')
+			if (format[i + 1] == '%')
+			{
+				_putchar('%');
+			}
+			else if (format[i + 1] == 'c')
 			{
 				_putchar(va_arg(list, int));
 			}
-			if (format[i + 1] == 's')
+			else if (format[i + 1] == 's')
 			{
 				str = va_arg(list, char *);
 				_putstring(str);
 			}
-			if (format[i + 1] == 'd')
+			else if (format[i + 1] == 'd')
 			{
 				_putint(va_arg(list, int));
 			}
-			va_end(list);
 			continue;
 		}
 		if (format[i - 1] != '%')
 			_putchar(format[i]);
 	}
+	va_end(list);
 	return (i - 1);
 }
